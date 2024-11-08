d9f410d1a5b4ec728f6e435f7c6deeb1076be54a
Sidharth-A-691 <sidharth69122@gmail.com>
2024-11-08 12:23:43 +0530

    first

--- src/main/java/com/finance/gateway/SpringCloudConfiguration.java
+++ src/main/java/com/finance/gateway/SpringCloudConfiguration.java
@@ -1,4 +1,4 @@
-package com.ust.gateway;
+package com.finance.gateway;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -15,9 +15,18 @@ public class SpringCloudConfiguration {
 		return
 				builder
 				.routes()
-				.route("helloservice", r->r.path("/rest/service/**")
-					.filters(f->f.addResponseHeader("X-Response-Header","World"))
-					.uri("http://localhost:8071"))
+				.route("userService", r->r.path("/finwiz/**")
+					.filters(f->f.addResponseHeader("X-Response-Header","user"))
+					.uri("http://localhost:8099"))
+				.route("transactionService",r->r.path("/finwiz/**")
+					.filters(f->f.addResponseHeader("X-Response-Header","transaction"))
+					.uri("http://localhost:8000"))
+				.route("mailService",r->r.path("/finwiz/**")
+						.filters(f->f.addResponseHeader("X-Response-Header","mail"))
+						.uri("http://localhost:6000"))
+				.route("goalService",r->r.path("/finwiz/**")
+						.filters(f->f.addResponseHeader("X-Response-Header","goal"))
+						.uri("http://localhost:8300"))
 				.build();
 	}
 }
